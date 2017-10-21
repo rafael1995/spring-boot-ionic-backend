a6f642ec0dd247b70c18f5d08298c1e71612f944
Rafael Alves Silva <rafael.alvessilva1995@gmail.com>
2017-10-21 13:35:06 -0300

    Ajustes no endpoint categoria/{id}

--- src/main/java/com/example/demo/cursomc/domain/Categoria.java
+++ src/main/java/com/example/demo/cursomc/domain/Categoria.java
@@ -1,4 +1,5 @@
 package com.example.demo.cursomc.domain;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,28 +10,30 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
+	@JsonManagedReference
 	@ManyToMany(mappedBy="categorias")
 	private List<Produto> produtos = new ArrayList<>();
 	
-	private Categoria() {
-		
+	public Categoria() {
 	}
+
 	public Categoria(Integer id, String nome) {
 		super();
 		this.id = id;
 		this.nome = nome;
 	}
 
-	
 	public Integer getId() {
 		return id;
 	}
@@ -45,6 +48,14 @@ public class Categoria implements Serializable {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 
 	@Override
@@ -70,12 +81,6 @@ public class Categoria implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
-	public List<Produto> getProdutos() {
-		return produtos;
-	}
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
 	}
 
 }
--- src/main/java/com/example/demo/cursomc/services/CategoriaService.java
+++ src/main/java/com/example/demo/cursomc/services/CategoriaService.java
@@ -6,14 +6,21 @@ import org.springframework.stereotype.Service;
 import com.example.demo.cursomc.domain.Categoria;
 import com.example.demo.cursomc.repositories.CategoriaRepository;
 
+import com.example.demo.cursomc.services.exceptions.ObjectNotFoundException;;
+
 @Service
 public class CategoriaService {
 	
 	@Autowired
 	private CategoriaRepository repo;
 	
+
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: " + Categoria.class.getName());
+		}
 		return obj;
 	}
 
